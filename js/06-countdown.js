c86be4f4c32c3995a80fce19090f7afea16531dc
OlenaZh <alyona.mesh@gmail.com>
2022-03-09 17:38:57 -0800

    cound down - done

---
+++ js/06-countdown.js
@@ -0,0 +1,12 @@
+let num = parseInt(prompt('Please enter a number'));
+for (num; num >= 0; num--) {
+    console.log(num)
+}
+
+//OR
+/* let num = parseInt(prompt('Please enter a number'));
+while (num > 0) {
+    console.log(num);
+    num--;
+}
+*/
