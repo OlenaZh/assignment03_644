79e210aaf59a2ecb79758ef694b00586d56bd682
OlenaZh <alyona.mesh@gmail.com>
2022-03-09 17:25:49 -0800

    marco/polo changes

--- js/05-marco-polo.js
+++ js/05-marco-polo.js
@@ -1,7 +1,7 @@
 let items = '';
 for (let i = 1; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-        console.log('Marco Polo!');
+    if (i % 15 === 0) {
+        console.log('Marco! Polo!');
     }
     else if (i % 3 === 0) {
         console.log('Marco!');
